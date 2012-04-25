d2fec5076990e4a1da0ff2c727ca7a5fe21f9af3
Andreas Neumann <andreas.neumann@an-it.com>
2012-04-25 20:58:06 +0200

    Vorträge auf einzelnen Seiten.

---
+++ app/controllers/vortrag_controller.rb
@@ -0,0 +1,36 @@
+#coding: utf-8
+class VortragController < ApplicationController
+  def index
+    @title="Vorträge"
+  end
+  
+  def der_mobile_opac_der_bayerischen_staatsbibliothek
+  end
+  
+  def mobile_opacs_bayerische_opacs_immer_und_ueberall
+  end
+  
+  def mobile_opacs_perspektiven_und_chancen
+  end
+  
+  def web_20_social_network_services
+
+  end
+
+  def web_20_in_bibliotheken
+
+  end
+
+  def soziale_netzwerke_bezirksjugendwerk_der_awo_westliches_westfalen
+
+  end
+  
+  def scala_workshop_bayerische_staatsbibliothek
+  end
+
+  def soziale_netzwerke_effektiv_nutzen_am_beispiel_von_facebook_twitter_und_google
+  end
+  
+  
+
+end
